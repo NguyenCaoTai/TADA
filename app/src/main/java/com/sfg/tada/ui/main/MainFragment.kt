7613ad90b280dd38830fad58644346587179b0a4
tai nc <tai.nguyen@tyme.com>
2021-12-26 15:24:11 +0700

    + Adding UI layout, UI logic, view model for main screen

--- app/src/main/java/com/sfg/tada/ui/main/MainFragment.kt
+++ app/src/main/java/com/sfg/tada/ui/main/MainFragment.kt
@@ -1,32 +1,199 @@
 package com.sfg.tada.ui.main
 
-import androidx.lifecycle.ViewModelProvider
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
 import com.sfg.tada.R
+import com.sfg.tada.databinding.MainFragmentBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
-class MainFragment : Fragment() {
+@AndroidEntryPoint
+class MainFragment : Fragment(),
+    OnMapReadyCallback {
 
     companion object {
         fun newInstance() = MainFragment()
+
+        private val TAG = MainFragment::class.java.simpleName
+        private const val DEFAULT_ZOOM = 15
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+        private const val KEY_CAMERA_POSITION = "camera_position"
+        private const val KEY_LOCATION = "location"
     }
 
-    private lateinit var viewModel: MainViewModel
+    lateinit var binding: MainFragmentBinding
+    private val viewModel: MainViewModel by viewModels()
+
+    private var map: GoogleMap? = null
+    private var cameraPosition: CameraPosition? = null
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val defaultLocation = LatLng(-33.8523341, 151.2106085)
+    private var locationPermissionGranted = false
+    private var lastKnownLocation: Location? = null
+    private var midLatLng: LatLng? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding = MainFragmentBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (savedInstanceState != null) {
+            lastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION)
+            cameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION)
+        }
+
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireActivity())
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        with(binding){
+            lifecycleOwner = this@MainFragment
+            viewmodel = viewModel
+
+            from.setOnClickListener {
+                viewModel.setBookingLocationFrom(midLatLng)
+                binding.bookingSwitcher.showNext()
+            }
+
+            to.setOnClickListener {
+                viewModel.setBookingLocationTo(midLatLng)
+                binding.bookingSwitcher.showNext()
+            }
+
+            book.setOnClickListener {
+                viewModel.booking()
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.bookingInfo
+                .collectLatest {
+                    Toast.makeText(requireContext(), "In Development", Toast.LENGTH_LONG).show()
+                }
+        }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        map?.let { map ->
+            outState.putParcelable(KEY_CAMERA_POSITION, map.cameraPosition)
+            outState.putParcelable(KEY_LOCATION, lastKnownLocation)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        this.map = map
+        this.map
+            ?.apply {
+                setOnCameraIdleListener { midLatLng = map.cameraPosition.target }
+                uiSettings.isMyLocationButtonEnabled = false
+            }
+        getLocationPermission()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getDeviceLocation() {
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        lastKnownLocation =
+                            task.result
+                                ?.also {
+                                    map?.moveCamera(
+                                        CameraUpdateFactory.newLatLngZoom(
+                                            LatLng(it.latitude, it.longitude),
+                                            DEFAULT_ZOOM.toFloat()
+                                        )
+                                    )
+
+                                    viewModel.init(LatLng(it.latitude, it.longitude))
+                                }
+                    } else {
+                        Log.d(TAG, "Current location is null. Using defaults.")
+                        Log.e(TAG, "Exception: %s", task.exception)
+                        map?.moveCamera(
+                            CameraUpdateFactory
+                                .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat())
+                        )
+                        map?.uiSettings?.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    private fun getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this.requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationPermissionGranted = true
+
+            getDeviceLocation()
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        locationPermissionGranted = false
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    locationPermissionGranted = true
+
+                    getDeviceLocation()
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/sfg/tada/ui/main/MainViewModel.kt
+++ app/src/main/java/com/sfg/tada/ui/main/MainViewModel.kt
@@ -1,7 +1,88 @@
 package com.sfg.tada.ui.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.android.gms.maps.model.LatLng
+import com.sfg.tada.data.model.BookingInfo
+import com.sfg.tada.data.model.PlaceInfo
+import com.sfg.tada.data.repo.InfoRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val infoRepo: InfoRepo
+) : ViewModel() {
+    private val _initLocation = PlaceInfo(-1.0, -1.0, "N/A", "N/A")
+    val selectedLocation = MutableStateFlow<PlaceInfo>(_initLocation)
+
+    private val _currentLocation = MutableSharedFlow<LatLng>(1)
+    private val _locationFrom = MutableSharedFlow<LatLng>(1)
+    private val _locationTo = MutableSharedFlow<LatLng>(1)
+    private val _booking = MutableSharedFlow<Unit>(1)
+
+    private val currentLocation: Flow<PlaceInfo> =
+        _currentLocation
+            .flatMapConcat { infoRepo.getPlaceInfo(it.latitude, it.longitude) }
+            .onEach { selectedLocation.tryEmit(it) }
+
+    val locationFrom: StateFlow<ViewState> =
+        _locationFrom
+            .flatMapConcat { infoRepo.getPlaceInfo(it.latitude, it.longitude) }
+            .onEach { selectedLocation.tryEmit(it) }
+            .map { ViewState.Fetched(it) }
+            .stateIn(
+                viewModelScope,
+                SharingStarted.WhileSubscribed(5000),
+                ViewState.Init(_initLocation.copy(name = "Set A"))
+            )
+
+    val locationTo: StateFlow<ViewState> =
+        _locationTo
+            .flatMapConcat { infoRepo.getPlaceInfo(it.latitude, it.longitude) }
+            .onEach { selectedLocation.tryEmit(it) }
+            .map { ViewState.Fetched(it) }
+            .stateIn(
+                viewModelScope,
+                SharingStarted.WhileSubscribed(5000),
+                ViewState.Init(_initLocation.copy(name = "Set B"))
+            )
+
+    val bookingInfo: SharedFlow<BookingInfo> =
+        _booking
+            .flatMapLatest { locationFrom.zip(locationTo) { from, to -> from to to } }
+            .filter { it.first is ViewState.Fetched && it.second is ViewState.Fetched }
+            .map { BookingInfo(it.first.info, it.second.info) }
+            .shareIn(
+                viewModelScope,
+                SharingStarted.WhileSubscribed(5000),
+                0
+            )
+
+    init {
+        viewModelScope.launch { currentLocation.collect() }
+    }
+
+    fun init(latLng: LatLng?) {
+        latLng?.let { _currentLocation.tryEmit(it) }
+    }
+
+    fun setBookingLocationFrom(latLng: LatLng?) {
+        latLng?.let { _locationFrom.tryEmit(it) }
+    }
+
+    fun setBookingLocationTo(latLng: LatLng?) {
+        latLng?.let { _locationTo.tryEmit(it) }
+    }
+
+    fun booking() {
+        _booking.tryEmit(Unit)
+    }
+}
+
+sealed class ViewState(val info: PlaceInfo) {
+    data class Init(val empty: PlaceInfo) : ViewState(empty)
+    data class Fetched(val fetched: PlaceInfo) : ViewState(fetched)
 }
